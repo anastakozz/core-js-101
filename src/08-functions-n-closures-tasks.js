35be452a6ba0030137ac0afc3aad7c883b44c7fb
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-07-02 12:53:12 +0300

    feat: solve partialargs

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -147,6 +147,11 @@ function retry(/* func, attempts */) {
  */
 function logger(/* func, logFunc */) {
   throw new Error('Not implemented');
+  // return (...args) => {
+  //   logFunc('start');
+  //   func(...args);
+  //   logFunc('end');
+  // };
 }
 
 
@@ -163,8 +168,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...arg) => fn(...args1, ...arg);
 }
 
 
